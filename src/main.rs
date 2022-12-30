9636a5bb1235d80ede506cd1a2da4a7ad4888068
Edgar B <39066502+Guekka@users.noreply.github.com>
2022-12-30 21:09:55 +0100

    fix: comment out main

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod parser;
 mod utils;
 
 fn main() {
-    parser::parse_instr_line("lsls r0, r1, #4").expect("Failed to parse");
+    //parser::parse_instr_line("lsls r0, r1, #4").expect("Failed to parse");
 }
