1e489a87cef6cb03224ec2a4cdb8545c922d99fb
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-01-29 19:53:53 +0100

    feat(cli): allow processing several files

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
-use parm_assembler::export_to_logisim;
-use std::env;
+use parm_assembler::{export_to_logisim, ExportError};
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
+use std::{env, fs};
 
 struct Args {
-    input: PathBuf,
+    input: Vec<PathBuf>,
 }
 
 fn parse_args() -> Args {
     let args: Vec<String> = env::args().collect();
     let binary = &args[0];
-    if args.len() != 2 {
+    if args.len() < 2 {
         panic!("Usage:{}  <input file>", binary);
     }
 
-    Args {
-        input: PathBuf::from(args[1].clone()),
-    }
+    let files = args
+        .into_iter()
+        .skip(1)
+        .map(PathBuf::from)
+        .flat_map(|path| {
+            if let Ok(entries) = fs::read_dir(&path) {
+                entries
+                    .filter_map(|entry| entry.ok())
+                    .map(|entry| entry.path())
+                    .collect::<Vec<_>>()
+            } else {
+                vec![path]
+            }
+        })
+        .filter(|path| path.is_file() && path.extension().map(|ext| ext == "s").unwrap_or(false))
+        .collect();
+
+    Args { input: files }
 }
 
 fn read_file(path: &Path) -> String {
@@ -32,16 +47,32 @@ fn write_file(path: &Path, contents: &str) {
     file.write_all(contents.as_bytes()).unwrap();
 }
 
+fn process_file(path: &Path) -> Result<(), ExportError> {
+    let contents = read_file(&path);
+
+    let output = export_to_logisim(&contents)?;
+    write_file(&path.with_extension("bin"), &output);
+
+    Ok(())
+}
+
 fn main() {
     let args = parse_args();
-    let contents = read_file(&args.input);
 
-    match export_to_logisim(&contents) {
-        Ok(output) => {
-            write_file(&args.input.with_extension("bin"), &output);
-        }
-        Err(e) => {
-            println!("Error: {}", e);
-        }
+    let (succeeded, failed): (Vec<_>, Vec<_>) = args
+        .input
+        .into_iter()
+        .map(|path| (process_file(path.as_ref()), path))
+        .partition(|(result, _)| result.is_ok());
+
+    for (result, path) in failed {
+        println!(
+            "Failed to process {}: {}",
+            path.display(),
+            result.unwrap_err()
+        );
+    }
+    for (_, path) in succeeded {
+        println!("Processed {}", path.display());
     }
 }
